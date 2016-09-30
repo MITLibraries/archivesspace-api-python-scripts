798d6e21c9620c32621c59c48e949348187cb069
ehanson8 <ehanson8@jh.edu>
2016-09-30 09:30:55 -0400

    script updates

---
+++ post.py
@@ -0,0 +1,18 @@
+import json
+import requests
+import secrets
+
+baseURL = secrets.baseURL
+user = secrets.user
+password = secrets.password
+
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+
+records = json.load(open('[JSON File]'))
+for i in range (0, len (records)):
+    record = json.dumps(records[i])
+    uri = records[i]['uri']
+    post = requests.post(baseURL + uri, headers=headers, data=record).json()
+    print post
