08356d3ed069dc1094e9a1c71984df4105d0dc98
ehanson8 <ehanson8@jh.edu>
2017-01-27 15:02:38 -0500

    Script updates

--- getPropertiesFromAgentsPeopleCSV.py
+++ getPropertiesFromAgentsPeopleCSV.py
@@ -10,11 +10,17 @@ password = secrets.password
 auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
 session = auth["session"]
 headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+print 'authenticated'
 
 endpoint = '/agents/people'
 arguments = '?page=1&page_size=3000'
 output = requests.get(baseURL + endpoint + arguments, headers=headers).json()
+records = output['results']
+
 f=csv.writer(open('asResults.csv', 'wb'))
-f.writerow(['uri']+['sort_name']+['authority_id']+['names'])
-for i in range (0, len (output['results'])):
-	f.writerow([json.dumps(output['results'][i]['uri']).replace('"','')]+[json.dumps(output['results'][i]['names'][0]['sort_name'], ensure_ascii=False).encode('utf-8').replace('"','')]+[json.dumps(output['results'][i]['names'][0].get('authority_id', '')).replace('"','')]+[json.dumps(output['results'][i]['names'], ensure_ascii=False).encode('utf8')])
+f.writerow(['uri']+['sort_name']+['authority_id'])
+for i in range (0, len (records)):
+	uri = records[i]['uri']
+	sort_name = records[i]['names'][0]['sort_name'].encode('utf-8')
+	authority_id = records[i]['names'][0].get('authority_id', '')
+	f.writerow([uri]+[sort_name]+[authority_id])
