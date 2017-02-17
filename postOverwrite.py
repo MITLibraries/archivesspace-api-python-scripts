173507a4e57fa6bdaf8373606b002e4507cbb46c
Eric Hanson <ehanson8@jhu.edu>
2017-02-17 15:20:11 -0500

    Update postOverwrite.py

--- postOverwrite.py
+++ postOverwrite.py
@@ -1,6 +1,9 @@
 import json
 import requests
 import secrets
+import time
+
+startTime = time.time()
 
 baseURL = secrets.baseURL
 user = secrets.user
@@ -16,3 +19,8 @@ for i in range (0, len (records)):
     uri = records[i]['uri']
     post = requests.post(baseURL + uri, headers=headers, data=record).json()
     print post
+
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Total script run time: ', '%d:%02d:%02d' % (h, m, s)
