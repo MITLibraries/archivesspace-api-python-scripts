673767a360d42dd895c7ff9f7ceb22c16657a9f9
ehanson8 <ehanson8@jh.edu>
2017-12-04 16:00:15 -0500

    updates

---
+++ resourcesWithNoBibNum.py
@@ -0,0 +1,36 @@
+import json
+import requests
+import secrets
+import time
+import csv
+
+startTime = time.time()
+
+baseURL = secrets.baseURL
+user = secrets.user
+password = secrets.password
+
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session':session, 'Content_Type':'application/json'}
+
+endpoint = '/repositories/3/resources?all_ids=true'
+
+ids = requests.get(baseURL + endpoint, headers=headers).json()
+
+f=csv.writer(open('resourcesWithNoBibs.csv', 'wb'))
+f.writerow(['uri'])
+
+for id in ids:
+    print id
+    uri = '/repositories/3/resources/'+str(id)
+    record = requests.get(baseURL + uri, headers=headers).json()
+    try:
+        print record['user_defined']['real_1']
+    except:
+        f.writerow([uri])
+
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print 'Total script run time: ', '%d:%02d:%02d' % (h, m, s)
