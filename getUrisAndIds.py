46591fdfcc5e988d031e33d34fc555214a91d75e
ehanson8 <ehanson8@jh.edu>
2017-12-05 16:31:28 -0500

    updates

--- getUrisAndIds.py
+++ getUrisAndIds.py
@@ -22,12 +22,16 @@ f.writerow(['ConCatID']+['id_0']+['id_1']+['id_2']+['id_3']+['id'])
 
 for id in ids:
     print id
+    endpoint = '/repositories/3/'+recordType+'/'+str(id)
     output = requests.get(baseURL + endpoint, headers=headers).json()
     try:
+        id_0 = output['id_0']
     except:
+        id_0 = ''
     try:
         id_1 = '.'+output['id_1']
     except:
+        id_1 = ''
     try:
         id_2 = '.'+output['id_2']
     except:
@@ -35,4 +39,6 @@ for id in ids:
     try:
         id_3 = '.'+output['id_3']
     except:
+        id_3 = ''
     ConCatID = id_0+id_1+id_2+id_3
+    f.writerow([ConCatID]+[id_0]+[id_1[1:]]+[id_2[1:]]+[id_3[1:]]+[endpoint])
