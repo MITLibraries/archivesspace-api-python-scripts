414feb3cfbbc4cca195175fe1ca8e43a85b7659d
mjanowiecki <32551917+mjanowiecki@users.noreply.github.com>
2019-05-29 09:50:31 -0400

    Update eadToCSV.py exceptions

--- eadToCsv.py
+++ eadToCsv.py
@@ -1,98 +1,25 @@
 import csv
+import lxml
 from bs4 import BeautifulSoup
 
-def extractValuesFromComponentLevel (componentLevel):
-        level = componentLevel.name
-        componentLevelLabel = componentLevel['level']
-        unittitle = componentLevel.find('did').find('unittitle').text.replace('\n','').encode('utf-8')
-        controlAccess = []
-        originationList = []
-        try:
-            physdesc = componentLevel.find('did').find('physdesc').text.replace('\n','').encode('utf-8')
-        except:
-            physdesc = ''
-        try:
-            unitdate = componentLevel.find('did').find('unitdate')
-            dateExpression = unitdate.text.encode('utf-8').replace('\n','').replace('              ',' ').replace('            ',' ').encode('utf-8')
-            try:
-                dateType = unitdate['type']
-            except:
-                dateType = ''
-            try:
-                dateNormal = unitdate['normal']
-                beginDate = dateNormal[:dateNormal.index('/')]
-                endDate = dateNormal[dateNormal.index('/')+1:]
-            except:
-                beginDate = ''
-                endDate = ''
-        except:
-            dateExpression = ''
-            dateType = ''
-            beginDate = ''
-            endDate = ''
-        try:
-            scopecontentElement = componentLevel.find('scopecontent').find_all('p')
-            scopecontent = ''
-            for paragraph in scopecontentElement:
-                paragraphText = paragraph.text.replace('\n','').replace('              ',' ').replace('            ',' ').encode('utf-8')
-                scopecontent = scopecontent + paragraphText
-        except:
-            scopecontent = ''
-        try:
-            subjects = componentLevel.find('controlaccess').find_all()
-            for subject in subjects:
-                controlAccess.append(subject.text.encode('utf-8'))
-        except:
-            subjects = ''
-        try:
-            container1 = componentLevel.find('did').find_all('container')[0].text.encode('utf-8')
-        except:
-            container1 = ''
-        try:
-            containerId1 = componentLevel.find('did').find_all('container')[0]['id']
-        except:
-            containerId1 = ''
-        try:
-            containerType1 = componentLevel.find('did').find_all('container')[0]['type']
-        except:
-            containerType1 = ''
-        try:
-            container2 = componentLevel.find('did').find_all('container')[1].text.encode('utf-8')
-        except:
-            container2 = ''
-        try:
-            containerId2 = componentLevel.find('did').find_all('container')[1]['id']
-        except:
-            containerId2 = ''
-        try:
-            containerType2 = componentLevel.find('did').find_all('container')[1]['type']
-        except:
-            containerType2 = ''
-        try:
-            originations = componentLevel.find('did').find_all('origination')
-            for origination in originations:
-                if origination.find()['role'] == 'spn':
-                    originationList.append(origination.text.encode('utf-8'))
-        except:
-            originationList = ''
-        global sortOrder
-        sortOrder += 1
-        f.writerow([sortOrder]+[level]+[componentLevelLabel]+[containerType1]+[container1]+[containerType2]+[container2]+[unittitle]+[physdesc]+[dateExpression]+[dateType]+[beginDate]+[endDate]+[scopecontent]+[controlAccess]+[originationList]+[containerId1]+[containerId2])
 
-filepath = ''
-fileName = 'Coll.004_20181012_144804_UTC__ead.xml'
-xml = open(filepath+fileName)
-
-f=csv.writer(open(filepath+'eadFields.csv', 'w'))
-f.writerow(['sortOrder']+['hierarchy']+['level']+['containerType1']+['container1']+['containerType2']+['container2']+['unittitle']+['physdesc']+['dateexpression']+['datetype']+['begindate']+['enddate']+['scopecontent']+['controlAccess']+['origination']+['containerId1']+['containerId2'])
-upperComponentLevels = BeautifulSoup(xml, 'lxml').find('dsc').find_all('c01')
-sortOrder = 0
-for upperComponentLevel in upperComponentLevels:
-    componentLevelLabel = upperComponentLevel['level']
-    unittitle = upperComponentLevel.find('did').find('unittitle').text.encode('utf-8').replace('\n','').replace('              ', ' ')
+def extractValuesFromcmpntLvl(cmpntLvl):
+    """Extract value from the specified component level of the record."""
+    level = cmpntLvl.name
+    cmpntLvlLabel = cmpntLvl['level']
+    unittitle = cmpntLvl.find('did').find('unittitle').text
+    unittitle = unittitle.replace('\n', '')
+    controlAccess = []
+    originationList = []
     try:
-        unitdate = upperComponentLevel.find('did').find('unitdate')
-        dateExpression = unitdate.text.encode('utf-8').replace('\n','').replace('              ',' ').replace('            ',' ').encode('utf-8')
+        physdesc = cmpntLvl.find('did').find('physdesc').text
+        physdesc = physdesc.replace('\n', '')
+    except:
+        physdesc = ''
+
+    try:
+        unitdate = cmpntLvl.find('did').find('unitdate')
+        dateExpression = unitdate.text.replace('\n', '')
         try:
             dateType = unitdate['type']
         except:
@@ -100,7 +27,7 @@ for upperComponentLevel in upperComponentLevels:
         try:
             dateNormal = unitdate['normal']
             beginDate = dateNormal[:dateNormal.index('/')]
-            endDate = dateNormal[dateNormal.index('/')+1:]
+            endDate = dateNormal[dateNormal.index('/') + 1:]
         except:
             beginDate = ''
             endDate = ''
@@ -110,40 +37,141 @@ for upperComponentLevel in upperComponentLevels:
         beginDate = ''
         endDate = ''
     try:
-        scopecontentElement = upperComponentLevel.find('scopecontent').find_all('p')
+        scopecontentElement = cmpntLvl.find('scopecontent')
+        scopecontentElement = scopecontentElement.find_all('p')
         scopecontent = ''
         for paragraph in scopecontentElement:
-            paragraphText = paragraph.text.replace('\\n','').replace('              ',' ').replace('            ',' ').encode('utf-8')
+            paragraphText = paragraph.text.replace('\n', '')
+            scopecontent = scopecontent + paragraphText
+    except:
+        scopecontent = ''
+    try:
+        subjects = cmpntLvl.find('controlaccess').find_all()
+        for subject in subjects:
+            controlAccess.append(subject.text)
+    except:
+        subjects = ''
+    try:
+        container1 = cmpntLvl.find('did')
+        container1 = container1.find_all('container')[0].text
+    except:
+        container1 = ''
+    try:
+        containerId1 = cmpntLvl.find('did')
+        containerId1 = containerId1.find_all('container')[0]['id']
+    except:
+        containerId1 = ''
+    try:
+        containerType1 = cmpntLvl.find('did')
+        containerType1 = containerType1.find_all('container')[0]['type']
+    except:
+        containerType1 = ''
+    try:
+        container2 = cmpntLvl.find('did')
+        container2 = container2.find_all('container')[1].text
+    except:
+        container2 = ''
+    try:
+        containerId2 = cmpntLvl.find('did')
+        containerId2 = containerId2.find_all('container')[1]['id']
+    except:
+        containerId2 = ''
+    try:
+        containerType2 = cmpntLvl.find('did')
+        containerType2 = containerType2.find_all('container')[1]['type']
+    except:
+        containerType2 = ''
+    try:
+        originations = cmpntLvl.find('did').find_all('origination')
+        for origination in originations:
+            if origination.find()['role'] == 'spn':
+                originationList.append(origination.text)
+    except:
+        originationList = ''
+    global sortOrder
+    sortOrder += 1
+    f.writerow([sortOrder] + [level] + [cmpntLvlLabel]
+               + [containerType1] + [container1] + [containerType2]
+               + [container2] + [unittitle] + [physdesc] + [dateExpression]
+               + [dateType] + [beginDate] + [endDate] + [scopecontent]
+               + [controlAccess] + [originationList] + [containerId1]
+               + [containerId2])
+
+
+filepath = ''
+fileName = 'MS.0236_20190528_183302_UTC__ead.xml'
+xml = open(filepath + fileName)
+
+f = csv.writer(open(filepath + 'eadFields.csv', 'w'))
+f.writerow(['sortOrder'] + ['hierarchy'] + ['level'] + ['containerType1']
+           + ['container1'] + ['containerType2'] + ['container2']
+           + ['unittitle'] + ['physdesc'] + ['dateexpression'] + ['datetype']
+           + ['begindate'] + ['enddate'] + ['scopecontent'] + ['controlAccess']
+           + ['origination'] + ['containerId1'] + ['containerId2'])
+uppercmpntLvls = BeautifulSoup(xml, 'lxml').find('dsc').find_all('c01')
+sortOrder = 0
+for uppercmpntLvl in uppercmpntLvls:
+    cmpntLvlLabel = uppercmpntLvl['level']
+    unittitle = uppercmpntLvl.find('did').find('unittitle')
+    unittitle = unittitle.text.replace('\n', '')
+    try:
+        unitdate = uppercmpntLvl.find('did').find('unitdate')
+        dateExpression = unitdate.text.replace('\n', '')
+        try:
+            dateType = unitdate['type']
+        except:
+            dateType = ''
+        try:
+            dateNormal = unitdate['normal']
+            beginDate = dateNormal[:dateNormal.index('/')]
+            endDate = dateNormal[dateNormal.index('/') + 1:]
+        except:
+            beginDate = ''
+            endDate = ''
+    except:
+        dateExpression = ''
+        dateType = ''
+        beginDate = ''
+        endDate = ''
+    try:
+        scopecontentElement = uppercmpntLvl.find('scopecontent')
+        scopecontentElement = scopecontentElement.find_all('p')
+        scopecontent = ''
+        for paragraph in scopecontentElement:
+            paragraphText = paragraph.text.replace('\\n', '')
             scopecontent = scopecontent + paragraphText
     except:
         scopecontent = ''
     sortOrder += 1
-    f.writerow([sortOrder]+['c01']+[componentLevelLabel]+['']+['']+['']+['']+[unittitle]+['']+[dateExpression]+[dateType]+[beginDate]+[endDate]+[scopecontent]+['']+['']+['']+[''])
+    f.writerow([sortOrder] + ['c01'] + [cmpntLvlLabel] + [''] + ['']
+               + [''] + [''] + [unittitle] + [''] + [dateExpression]
+               + [dateType] + [beginDate] + [endDate] + [scopecontent] + ['']
+               + [''] + [''] + [''])
 
-    componentLevelArray = upperComponentLevel.find_all('c02')
-    for componentLevel in componentLevelArray:
-        extractValuesFromComponentLevel(componentLevel)
-        componentLevelArray = componentLevel.find_all('c03')
-        for componentLevel in componentLevelArray:
-            extractValuesFromComponentLevel(componentLevel)
-            componentLevelArray = componentLevel.find_all('c04')
-            for componentLevel in componentLevelArray:
-                extractValuesFromComponentLevel(componentLevel)
-                componentLevelArray = componentLevel.find_all('c05')
-                for componentLevel in componentLevelArray:
-                    extractValuesFromComponentLevel(componentLevel)
-                    componentLevelArray = componentLevel.find_all('c06')
-                    for componentLevel in componentLevelArray:
-                        extractValuesFromComponentLevel(componentLevel)
-                        componentLevelArray = componentLevel.find_all('c07')
-                        for componentLevel in componentLevelArray:
-                            extractValuesFromComponentLevel(componentLevel)
-                            componentLevelArray = componentLevel.find_all('c08')
-                            for componentLevel in componentLevelArray:
-                                extractValuesFromComponentLevel(componentLevel)
-                                componentLevelArray = componentLevel.find_all('c09')
-                                for componentLevel in componentLevelArray:
-                                    extractValuesFromComponentLevel(componentLevel)
-                                    componentLevelArray = componentLevel.find_all('c10')
-                                    for componentLevel in componentLevelArray:
-                                        extractValuesFromComponentLevel(componentLevel)
+    cmpntLvlArray = uppercmpntLvl.find_all('c02')
+    for cmpntLvl in cmpntLvlArray:
+        extractValuesFromcmpntLvl(cmpntLvl)
+        cmpntLvlArray = cmpntLvl.find_all('c03')
+        for cmpntLvl in cmpntLvlArray:
+            extractValuesFromcmpntLvl(cmpntLvl)
+            cmpntLvlArray = cmpntLvl.find_all('c04')
+            for cmpntLvl in cmpntLvlArray:
+                extractValuesFromcmpntLvl(cmpntLvl)
+                cmpntLvlArray = cmpntLvl.find_all('c05')
+                for cmpntLvl in cmpntLvlArray:
+                    extractValuesFromcmpntLvl(cmpntLvl)
+                    cmpntLvlArray = cmpntLvl.find_all('c06')
+                    for cmpntLvl in cmpntLvlArray:
+                        extractValuesFromcmpntLvl(cmpntLvl)
+                        cmpntLvlArray = cmpntLvl.find_all('c07')
+                        for cmpntLvl in cmpntLvlArray:
+                            extractValuesFromcmpntLvl(cmpntLvl)
+                            cmpntLvlArray = cmpntLvl.find_all('c08')
+                            for cmpntLvl in cmpntLvlArray:
+                                extractValuesFromcmpntLvl(cmpntLvl)
+                                cmpntLvlArray = cmpntLvl.find_all('c09')
+                                for cmpntLvl in cmpntLvlArray:
+                                    extractValuesFromcmpntLvl(cmpntLvl)
+                                    cmpntLvlArray = cmpntLvl.find_all('c10')
+                                    for cmpntLvl in cmpntLvlArray:
+                                        extractValuesFromcmpntLvl(cmpntLvl)
