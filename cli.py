e2bf00ae25e8a45b133bfe38c60c7129da1cfe17
Eric Hanson <ehanson@mit.edu>
2019-08-30 12:56:51 -0400

    add minimal click command issue #23

---
+++ cli.py
@@ -0,0 +1,23 @@
+import click
+from asaps import models
+from asnake.client import ASnakeClient
+
+
+@click.command()
+@click.option('--url', envvar='ARCHIVESSPACE_URL')
+@click.option('-u', '--username', prompt='Enter username',
+              help='The username for authentication.')
+@click.option('-p', '--password', prompt='Enter password',
+              hide_input=True,
+              help='The password for authentication.')
+def main(url, username, password):
+    print(url, username, password)
+    client = ASnakeClient(baseurl=url, username=username,
+                          password=password)
+    # for client verification, will remove later
+    record = client.get('/repositories/2/resources/423').json()
+    print(record)
+
+
+if __name__ == '__main__':
+    main()
