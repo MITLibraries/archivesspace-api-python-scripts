e0424423b5a6dd9e3b7a1e443a9cddd4e2084513
Eric Hanson <ehanson@mit.edu>
2019-09-24 10:19:15 -0400

    get all records method

--- asaps/cli.py
+++ asaps/cli.py
@@ -19,61 +19,77 @@ def main(url, username, password):
     as_ops = models.AsOperations(client)
     start_time = time.time()
 
-    rec_types = ['resource', 'archival_object']
-    corr_dict = {'IASC': 'DDC'}
-    # corr_dict = {'Institute Archives and Special Collections':
-    #              'Department of Distinctive Collections'}
-    # corr_dict = {'the Institute Archives': 'Distinctive Collections'}
-    # corr_dict = {'Institute Archives': 'Distinctive Collections'}
-    # corr_dict = {'IASC': 'DDC',
-    #              'Institute Archives and Special Collections':
-    #              'Department of Distinctive Collections',
-    #              'the Institute Archives': 'Distinctive Collections'}  # ,
-    #              # 'Institute Archives': 'Distinctive Collections'}
-
-    error_uris = ['/repositories/2/resources/424',
-                  '/repositories/2/resources/1233',
-                  '/repositories/2/resources/377',
-                  '/repositories/2/resources/356',
-                  '/repositories/2/resources/228',
-                  '/repositories/2/resources/658',
-                  '/repositories/2/resources/635',
-                  '/repositories/2/resources/704',
-                  '/repositories/2/resources/202',
-                  '/repositories/2/resources/586']
-
-    skipped_resources = ['/repositories/2/resources/535',
-                         '/repositories/2/resources/41',
-                         '/repositories/2/resources/111',
-                         '/repositories/2/resources/367',
-                         '/repositories/2/resources/231',
-                         '/repositories/2/resources/561',
-                         '/repositories/2/resources/563',
-                         '/repositories/2/resources/103']
-    skipped_aos = []
-    for uri in skipped_resources:
-        aolist = as_ops.get_aos_for_resource(uri)
-        skipped_aos.append(aolist)
-    skipped_uris = error_uris + skipped_resources + skipped_aos
     csv_data = []
-    note_types = ['userestrict', 'acqinfo']
-    counter = 0
-    for rec_type in rec_types:
-        for old, new in corr_dict.items():
-            results = as_ops.search(old, '2', rec_type)
-            for result in results:
-                counter += 1
-                uri = result['uri']
-                if uri not in skipped_uris:
-                    for note_type in note_types:
-                        print(old, rec_type, note_type, counter)
-                        rec_obj = as_ops.get_record(uri)
-                        rec_obj = models.filter_note_type(as_ops, csv_data,
-                                                          rec_obj, note_type,
-                                                          'find_replace_test',
-                                                          old, new)
-            else:
-                print(uri, ' skipped')
+    rec_type = 'resource'
+    repo_id = '2'
+    note_type = 'prefercite'
+    old = 'the'
+    new = 'that'
+    ids, endpoint = as_ops.get_all_records(rec_type, repo_id)
+    print(ids)
+    for id in ids:
+        uri = f'{endpoint}/{id}'
+        rec_obj = as_ops.get_record(uri)
+        rec_obj = models.filter_note_type(as_ops, csv_data,
+                                          rec_obj, note_type,
+                                          'find_replace_test',
+                                          old, new)
+
+    # rec_types = ['resource', 'archival_object']
+    # corr_dict = {'IASC': 'DDC'}
+    # # corr_dict = {'Institute Archives and Special Collections':
+    # #              'Department of Distinctive Collections'}
+    # # corr_dict = {'the Institute Archives': 'Distinctive Collections'}
+    # # corr_dict = {'Institute Archives': 'Distinctive Collections'}
+    # # corr_dict = {'IASC': 'DDC',
+    # #              'Institute Archives and Special Collections':
+    # #              'Department of Distinctive Collections',
+    # #              'the Institute Archives': 'Distinctive Collections'}  # ,
+    # #              # 'Institute Archives': 'Distinctive Collections'}
+    #
+    # error_uris = ['/repositories/2/resources/424',
+    #               '/repositories/2/resources/1233',
+    #               '/repositories/2/resources/377',
+    #               '/repositories/2/resources/356',
+    #               '/repositories/2/resources/228',
+    #               '/repositories/2/resources/658',
+    #               '/repositories/2/resources/635',
+    #               '/repositories/2/resources/704',
+    #               '/repositories/2/resources/202',
+    #               '/repositories/2/resources/586']
+    #
+    # skipped_resources = ['/repositories/2/resources/535',
+    #                      '/repositories/2/resources/41',
+    #                      '/repositories/2/resources/111',
+    #                      '/repositories/2/resources/367',
+    #                      '/repositories/2/resources/231',
+    #                      '/repositories/2/resources/561',
+    #                      '/repositories/2/resources/563',
+    #                      '/repositories/2/resources/103']
+    # skipped_aos = []
+    # for uri in skipped_resources:
+    #     aolist = as_ops.get_aos_for_resource(uri)
+    #     skipped_aos.append(aolist)
+    # skipped_uris = error_uris + skipped_resources + skipped_aos
+    # csv_data = []
+    # note_types = ['userestrict', 'acqinfo']
+    # counter = 0
+    # for rec_type in rec_types:
+    #     for old, new in corr_dict.items():
+    #         results = as_ops.search(old, '2', rec_type)
+    #         for result in results:
+    #             counter += 1
+    #             uri = result['uri']
+    #             if uri not in skipped_uris:
+    #                 for note_type in note_types:
+    #                     print(old, rec_type, note_type, counter)
+    #                     rec_obj = as_ops.get_record(uri)
+    #                     rec_obj = models.filter_note_type(as_ops, csv_data,
+    #                                                       rec_obj, note_type,
+    #                                                       'find_replace_test',
+    #                                                       old, new)
+    #         else:
+    #             print(uri, ' skipped')
     if len(csv_data) != 0:
         models.create_csv(csv_data, 'replace_str')
     else:
