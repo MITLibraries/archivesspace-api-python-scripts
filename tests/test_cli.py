0afb88f2260ca9c69f1a9ebed2a73d61a9bd953b
Eric Hanson <ehanson@mit.edu>
2020-02-28 11:32:48 -0500

    more cli tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,4 +1,5 @@
 from click.testing import CliRunner
+import csv
 import pytest
 import requests_mock
 
@@ -12,6 +13,28 @@ def runner():
 
 def test_report(runner):
     """Test report command."""
+    with requests_mock.Mocker() as m:
+        json_object1 = {'session': 'abcdefg1234567'}
+        json_object2 = {'id_0': 'AB', 'id_1': '123', 'title': 'Test title',
+                        'uri': '/repositories/0/resources/1234', 'notes':
+                        [{'type': 'acqinfo', 'subnotes': [{'content':
+                         'test value'}]}]}
+        base_url = 'mock://mock.mock/users/test/login'
+        ids_url = '/repositories/0/resources?all_ids=true'
+        item_url = '/repositories/0/resources/1234'
+        m.post(base_url, json=json_object1)
+        m.get(ids_url, json=['1234'])
+        m.get(item_url, json=json_object2)
+        result = runner.invoke(main,
+                               ['--url', 'mock://mock.mock',
+                                '--username', 'test',
+                                '--password', 'testpass',
+                                'report',
+                                '--repo_id', '0',
+                                '--rec_type', 'resource',
+                                '--field', 'acqinfo',
+                                ])
+        assert result.exit_code == 0
 
 
 def test_find(runner):
@@ -23,8 +46,8 @@ def test_find(runner):
                          'test value'}]}]}
         json_object3 = {'status': 'Updated'}
         base_url = 'mock://mock.mock/users/test/login'
-        search_url = f'/repositories/0/search?'
-        item_url = f'/repositories/0/resources/1234'
+        search_url = '/repositories/0/search?'
+        item_url = '/repositories/0/resources/1234'
         m.post(base_url, json=json_object1)
         m.get(search_url, json=[json_object2])
         m.get(item_url, json=json_object2)
@@ -46,3 +69,29 @@ def test_find(runner):
 
 def test_updatedigobj(runner):
     """Test report command."""
+    with requests_mock.Mocker() as m:
+        with runner.isolated_filesystem():
+            with open('mapping.csv', 'w') as f:
+                writer = csv.writer(f)
+                writer.writerow(['do_uri'] + ['link'])
+                writer.writerow(['/repositories/0/digital_objects/1234'] +
+                                ['new_content_link'])
+            json_object1 = {'session': 'abcdefg1234567'}
+            json_object2 = {'uri': '/repositories/0/digital_objects/1234',
+                            'file_versions': [{'file_uri':
+                                              'old_content_link'}]}
+            json_object3 = {'status': 'Updated'}
+            base_url = 'mock://mock.mock/users/test/login'
+            item_url = '/repositories/0/digital_objects/1234'
+            m.post(base_url, json=json_object1)
+            m.get(item_url, json=json_object2)
+            m.post(item_url, json=json_object3)
+            result = runner.invoke(main,
+                                   ['--url', 'mock://mock.mock',
+                                    '--username', 'test',
+                                    '--password', 'testpass',
+                                    'updatedigobj',
+                                    '--dry_run', 'False',
+                                    '--mapping_csv', 'mapping.csv'
+                                    ])
+    assert result.exit_code == 0
